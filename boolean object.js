a6872557abe00f014a5d4e801dbb4e0b550cc0e3
Rama krishna <rk4053727@gmail.com>
2024-10-25 16:36:21 +0530

    Create boolean object.js

---
+++ boolean object.js
@@ -0,0 +1,10 @@
+const a = true;
+
+// creating a boolean object
+const b = new Boolean(true);
+
+console.log(a); // true
+console.log(b); // true
+
+console.log(typeof a); // "boolean"
+console.log(typeof b); // "object"
