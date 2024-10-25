849e34074e6e6b1cee220e2bc573d4aa9adcf36c
Rama krishna <rk4053727@gmail.com>
2024-10-25 16:35:03 +0530

    Create example4.js

---
+++ example4.js
@@ -0,0 +1,16 @@
+let result;
+// empty string
+result = Boolean('');
+console.log(result); // false
+
+result = Boolean(0);
+console.log(result); // false
+
+result = Boolean(undefined);
+console.log(result); // false
+
+result = Boolean(null);
+console.log(result); // false
+
+result = Boolean(NaN);
+console.log(result); // false
