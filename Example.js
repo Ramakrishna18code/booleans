63e07d08239094536524adf5c6405caf0c84f53c
Rama krishna <rk4053727@gmail.com>
2024-10-25 16:14:36 +0530

    Create Example.js

---
+++ Example.js
@@ -0,0 +1,12 @@
+const a="true";
+console.log(typeof a);   //string
+
+
+
+//equal to operator == returns true 
+//example
+
+console.log(5==6); //flase 
+console.log(5!=6); //true  //not equal to
+console.log(true&& false);  //false   //logical AND if both operands are true
+
