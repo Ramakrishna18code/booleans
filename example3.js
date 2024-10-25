a2ddd28318c07686f2eeaf26d12e70541f4ec51d
Rama krishna <rk4053727@gmail.com>
2024-10-25 16:32:53 +0530

    Create example3.js

---
+++ example3.js
@@ -0,0 +1,13 @@
+let result;
+result = 20;
+console.log(Boolean(result)); 
+console.log(typeof Boolean(result)); 
+
+result = -20;
+console.log(Boolean(result)); 
+
+result = 'hello';
+console.log(Boolean(result)); 
+
+result = {a: 1};
+console.log(Boolean(result));
